b441a7c0f38b5907a7bb8fe45fe5d0509ca6b319
Happy Slime <qjaxoddl522@naver.com>
2024-09-16 00:16:09 +0900

    FSM 기반 이동 및 행동 개선

--- Assets/Character/Minimi_Control.cs
+++ Assets/Character/Minimi_Control.cs
@@ -1,78 +1,183 @@
 using Spine.Unity;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-using System.Linq;
+using Random = UnityEngine.Random;
+
+public abstract class FSMBase
+{
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
+}
+
+public class IdleState : FSMBase
+{
+    readonly Minimi_Control minimi;
+
+    public IdleState(Minimi_Control minimi)
+    {
+        this.minimi = minimi;
+    }
+
+    public override void EnterState()
+    {
+        minimi.remainUpdateTime = Random.Range(minimi.updateIntervalLeft, minimi.updateIntervalRight);
+    }
+    public override void ExitState() { }
+    public override void UpdateState()
+    {
+        minimi.remainUpdateTime -= Time.deltaTime;
+    }
+}
+
+public class MoveTargetState : FSMBase
+{
+    readonly Minimi_Control minimi;
+
+    public MoveTargetState(Minimi_Control minimi)
+    {
+        this.minimi = minimi;
+    }
+
+    public override void EnterState()
+    {
+        minimi.agent.SetDestination(minimi.targetPos);
+    }
+    public override void ExitState()
+    {
+        Debug.Log("도착");
+    }
+    public override void UpdateState() { }
+}
+
+public class MoveFreeState : FSMBase
+{
+    readonly Minimi_Control minimi;
+
+    public MoveFreeState(Minimi_Control minimi)
+    {
+        this.minimi = minimi;
+    }
+
+    public override void EnterState()
+    {
+        Vector3 randomPos = Minimi_Spawner.GetRandomPosOnNavMesh(minimi.transform.position, minimi.updateDistance);
+        minimi.agent.SetDestination(randomPos);
+    }
+    public override void ExitState() { }
+    public override void UpdateState() { }
+}
+
 
 public class Minimi_Control : MonoBehaviour
 {
-    public float updateInterval;          // 목표지점 변경 주기
+    public float updateIntervalLeft;      // 목표지점 변경 주기
+    public float updateIntervalRight;     
     public float updateDistance;          // 목표지점 변경 범위
-    public string[] exceptAnimationNames; // 제외할 애니메이션
+    public Vector3 targetPos;
 
-    NavMeshAgent agent;
-    float lastUpdateTime;           // 마지막으로 목표지점 갱신한 시간
-    Vector3 initialScale;
+    public NavMeshAgent agent;
+    public Vector2 mousePos;
+    public float remainUpdateTime;        // 목표지점 갱신까지 남은 시간
 
     SkeletonAnimation skAnim;
-    List<string> animationNames;
+    MeshRenderer meshRenderer;
+    Vector3 initialScale;
 
-    Vector2 mousePos;
+    FSMBase state;
+    public bool IsIdle
+    {
+        get
+        {
+            return state.GetType() == typeof(IdleState);
+        }
+    }
+    public bool IsMoveTarget
+    {
+        get
+        {
+            return state.GetType() == typeof(MoveTargetState);
+        }
+    }
+    public bool IsMoveFree
+    {
+        get
+        {
+            return state.GetType() == typeof(MoveFreeState);
+        }
+    }
+
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        skAnim = GetComponent<SkeletonAnimation>();
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
 
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        agent.autoRepath = false;
         agent.updateRotation = false;
         agent.updateUpAxis = false;
-        lastUpdateTime = updateInterval;
         initialScale = transform.localScale;
 
-        // 애니메이션 이름 목록 불러오기
-        skAnim = GetComponent<SkeletonAnimation>();
-        animationNames = new List<string>();
-        Spine.SkeletonData skeletonData = skAnim.Skeleton.Data;
-        foreach (var animation in skeletonData.Animations)
-        {
-            if (!exceptAnimationNames.Any(except => animation.Name.ToLower().Contains(except.ToLower())))
-                animationNames.Add(animation.Name);
-        }
+        if (state == null)
+            state = new IdleState(this);
     }
 
     void Update()
     {
         if (Input.GetMouseButtonUp(0))
         {
-            string randomAnimation = animationNames[Random.Range(0, animationNames.Count)];
-            skAnim.AnimationState.SetAnimation(0, randomAnimation, true);
+            ChangeRandomAnim();
             //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             //agent.SetDestination(mousePos);
         }
 
-        lastUpdateTime += Time.deltaTime;
-        if (lastUpdateTime >= updateInterval)
+        if (IsMoveTarget || IsMoveFree)
         {
-            Vector3 randomPos = GetRandomPosOnNavMesh(updateDistance);
-            agent.SetDestination(randomPos);
-            lastUpdateTime = 0;
+            if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!IsIdle)
+                    ChangeState<IdleState>();
+            }
         }
+        else if (remainUpdateTime <= 0)
+        {
+            if (!IsMoveFree)
+                ChangeState<MoveFreeState>();
+        }
+        UpdateState();
 
         // 이동 방향따라 반전
         if (agent.velocity.x > 0)
             transform.localScale = new Vector3(-initialScale.x, initialScale.y, initialScale.z);
         else if (agent.velocity.x < 0)
             transform.localScale = new Vector3(initialScale.x, initialScale.y, initialScale.z);
+        // 순서 조절
+        meshRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
     }
 
-    Vector3 GetRandomPosOnNavMesh(float distance)
+    void UpdateState()
     {
-        Vector3 randomDirection = Random.insideUnitCircle * distance;
-        randomDirection += transform.position;
+        state.UpdateState();
+    }
 
-        // NavMesh 위의 랜덤 위치를 hit에 저장
-        NavMeshHit hit;
-        if (NavMesh.SamplePosition(randomDirection, out hit, distance, NavMesh.AllAreas))
-            return hit.position;
-        else
-            return transform.position;
+    public void ChangeState<T>() where T : FSMBase
+    {
+        if (state != null && state.GetType() != typeof(T))
+            state.ExitState();
+
+        T t = (T)Activator.CreateInstance(typeof(T), args: this);
+        this.state = t;
+        t.EnterState();
+    }
+
+    public void ChangeRandomAnim()
+    {
+        string randomAnimation = Minimi_Spawner.animationNames[Random.Range(0, Minimi_Spawner.animationNames.Count)];
+        skAnim.AnimationState.SetAnimation(0, randomAnimation, true);
     }
 }
--- Assets/Character/Minimi_Spawner.cs
+++ Assets/Character/Minimi_Spawner.cs
@@ -2,24 +2,84 @@ using Spine.Unity;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class Minimi_Spawner : MonoBehaviour
 {
+    public string[] minimiNames;          // 생성할 사도 이름들
     public string[] exceptAnimationNames; // 제외할 애니메이션
+    public GameObject minimiPrefab;
+    public GameObject minimiSpawnPointParent;
+    public GameObject minimiCenterPointParent;
+    public GameObject minimiParent;
 
-    SkeletonAnimation skAnim;
-    List<string> animationNames;
+    SkeletonAnimation skAnim_Reading;
+    public static List<string> animationNames;
+    List<Vector3> spawnPoints;
+    List<Vector3> centerPoints;
 
     void Start()
     {
         // 애니메이션 이름 목록 불러오기
-        skAnim = GetComponent<SkeletonAnimation>();
+        skAnim_Reading = GetComponent<SkeletonAnimation>();
         animationNames = new List<string>();
-        Spine.SkeletonData skeletonData = skAnim.Skeleton.Data;
+        Spine.SkeletonData skeletonData = skAnim_Reading.Skeleton.Data;
         foreach (var animation in skeletonData.Animations)
         {
             if (!exceptAnimationNames.Any(except => animation.Name.ToLower().Contains(except.ToLower())))
                 animationNames.Add(animation.Name);
         }
+
+        // 생성지점 추가
+        spawnPoints = new List<Vector3>();
+        foreach(Transform child in minimiSpawnPointParent.transform)
+        {
+            spawnPoints.Add(child.position);
+        }
+
+        // 중심지점 추가
+        centerPoints = new List<Vector3>();
+        foreach (Transform child in minimiCenterPointParent.transform)
+        {
+            centerPoints.Add(child.position);
+        }
+
+        // 사도 생성
+        foreach (string name in minimiNames)
+        {
+            Vector3 spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Count)];
+            GameObject minimi = Instantiate(minimiPrefab, spawnPoint, Quaternion.identity, minimiParent.transform);
+            minimi.name = name;
+
+            SkeletonAnimation skAnim = minimi.GetComponent<SkeletonAnimation>();
+            skAnim.Skeleton.SetSkin(name);
+
+            Minimi_Control control = minimi.GetComponent<Minimi_Control>();
+            control.ChangeRandomAnim();
+            Vector3 centerPoint = centerPoints[Random.Range(0, centerPoints.Count)];
+            control.targetPos = GetRandomPosOnNavMesh(centerPoint, 0.5f);
+            control.ChangeState<MoveTargetState>();
+        }
+    }
+
+    public static Vector3 GetRandomPosOnNavMesh(Vector2 point, float range)
+    {
+        // NavMesh 위의 랜덤 위치를 hit에 저장
+        NavMeshHit hit;
+        int t = 0;
+        while (t < 30)
+        {
+            Vector2 randomDirection = point + Random.insideUnitCircle * range;
+            if (NavMesh.SamplePosition(randomDirection, out hit, range, NavMesh.AllAreas))
+            {
+                NavMeshHit edgeHit;
+                if (NavMesh.FindClosestEdge(hit.position, out edgeHit, NavMesh.AllAreas))
+                    if (edgeHit.distance > 0.1)
+                        return hit.position;
+            }
+            t++;
+        }
+        return point;
     }
 }
